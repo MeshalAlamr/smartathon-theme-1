8cc5c60d0f4b6851cd029baf38fc2e9dc00ebb28
meshal <meshal037@gmail.com>
2023-01-22 04:09:33 +0300

    Download model weights automatically

--- inference.py
+++ inference.py
@@ -4,13 +4,18 @@ import torch
 import argparse 
 import os
 import cv2
+
+# ignore warnings
 import warnings
 warnings.filterwarnings("ignore")
+
 # to log the results using current date and time
 from datetime import datetime
 
 # progress bar
 from tqdm import tqdm
+
+PATTRN_WEIGHTS = 'https://github.com/MeshalAlamr/smartathon-theme-1/raw/main/models/pattrn/weights/best.pt'
 
 def prepare_results(model_name, n_segments):
     print("Resizing images to 960x540 and saving them to resized_dataset/images if they do not exist...")
@@ -33,6 +38,11 @@ def prepare_results(model_name, n_segments):
     # realase the GPU memory
     torch.cuda.empty_cache()
 
+    # if the pattrn model weights do not exist, or if they are corrupted, download them
+    if not os.path.exists(f'models/pattrn/weights/best.pt') or os.stat('models/pattrn/weights/best.pt').st_size < 1E8:
+        print("Downloading model weights...")
+        torch.hub.download_url_to_file(PATTRN_WEIGHTS, 'models/pattrn/weights/best.pt', progress=True)
+            
     # Load Model
     model = torch.hub.load('ultralytics/yolov5', 'custom', path=f'models/{model_name}/weights/best.pt')  # local model
 
